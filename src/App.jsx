ea0e0ad73fbbc8d78bfe7ffb7a3b9d353d4fc4dd
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-09-18 16:00:31 +0700

    Add React Router package and implement some example routes - Home - Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
