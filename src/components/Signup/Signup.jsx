8ccb7b07d6c28d5f9d67a1c6ce97e67c63d4ba73
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-09-23 22:50:39 +0700

    Implement Sign up component

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -5,11 +5,32 @@ import { useState } from "react";
 
 function Signup(props) {
   const [username, setUsername] = useState("");
-  const [authResults, setAuthResults] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setrepeatPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [authResults, setAuthResults] = useState([]);
 
   function typingUsername(e) {
     setUsername(e.target.value);
   }
+
+  function typingPassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function typingrepeatPassword(e) {
+    setrepeatPassword(e.target.value);
+  }
+
+  function typingFullname(e) {
+    setFullname(e.target.value);
+  }
+
+  function typingEmail(e) {
+    setEmail(e.target.value);
+  }
+
   async function handleSignup(formData) {
     const username = formData.get("username");
     const password = formData.get("password");
@@ -18,17 +39,23 @@ function Signup(props) {
     const email = formData.get("email");
 
     try {
-      const response = await fetch("http://localhost:3000/auth/signup", {
+      const response = await fetch("http://localhost:3000/auth/sign-up", {
         method: "POST",
-        body: JSON.stringify({ username: username, password: password }),
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          repeat_password: repeatPassword,
+          name: fullname,
+          email: email,
+        }),
         headers: {
           "Content-type": "application/json",
         },
       });
 
       const result = await response.json();
-      if (result.info) {
-        setAuthResults(result.info.message);
+      if (result.errors && result.errors.length > 0) {
+        setAuthResults(result);
       }
 
       localStorage.setItem("access_token", result.token);
@@ -55,7 +82,10 @@ function Signup(props) {
                 Sign up today to let others know your thoughts
               </p>
             </div>
-            <form action={() => handleSignup()} className="flex flex-col">
+            <form
+              action={(formData) => handleSignup(formData)}
+              className="flex flex-col"
+            >
               <div className="flex flex-col">
                 <label htmlFor="username" className="text-zinc-500">
                   Username:
@@ -82,6 +112,8 @@ function Signup(props) {
                   required
                   minLength={8}
                   maxLength={30}
+                  onChange={typingPassword}
+                  value={password}
                 />
               </div>
               <div className="flex flex-col mt-3">
@@ -96,6 +128,8 @@ function Signup(props) {
                   required
                   minLength={8}
                   maxLength={30}
+                  onChange={typingrepeatPassword}
+                  value={repeatPassword}
                 />
               </div>
 
@@ -110,11 +144,13 @@ function Signup(props) {
                   id="fullname"
                   className="p-1.5 w-full"
                   required
+                  onChange={typingFullname}
+                  value={fullname}
                 />
               </div>
 
               <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
+                <label htmlFor="email" className="text-zinc-500">
                   Email:
                 </label>
                 <input
@@ -123,10 +159,21 @@ function Signup(props) {
                   id="email"
                   className="p-1.5 w-full"
                   required
+                  onChange={typingEmail}
+                  value={email}
                 />
               </div>
 
-              {authResults && <p className="text-red-500">{authResults}</p>}
+              {authResults.errors && (
+                <div>
+                  <p className="text-red-500">{authResults.title}</p>
+                  <ul className="text-red-500 list-disc pl-4">
+                    {authResults.errors.map((rs, i) => {
+                      return <li key={i}>{rs.msg}</li>;
+                    })}
+                  </ul>
+                </div>
+              )}
               <button type="submit" className="mt-5 bg-blue-700 text-white">
                 Sign up now
               </button>
